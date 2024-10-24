c7ef6b2b0e6c252389696e34c1216beea43bdf7b
webguru0110 <dmytromishchenkov@gmail.com>
2024-10-24 17:44:24 +0300

    homepage basic completed

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -12,14 +12,15 @@ import Typography from '@mui/material/Typography';
 
 
 const styles = {
-    flex: { display: 'flex' },
-    flexColumn: { display: 'flex', flexDirection: 'column' },
+    flex: { display: 'flex', justifyContent: 'space-around' },
+    flexColumn: { display: 'flex', flexDirection: 'column', marginInline: '5vw' },
+    flexColumnXsmall: { alignItems:'center', display: 'flex', flexDirection: 'column', marginInline: '5vw' },
     leftCol: { flex: 1, marginRight: '0.5em' },
     midCol: { flex: 1 },
     rightCol: { flex: 1, marginLeft: '0.5em' },
     singleCol: { marginTop: '1em', marginBottom: '1em' },
-    header: { marginTop: '1em', paddingInline:'10vw', fontSize: '30px', textAlign: 'center'},
-    subHeader: { marginBottom: '5em', paddingInline:'6vw', fontSize: '20px', textAlign: 'center'},
+    header: { marginTop: '1em', marginInline:'20vw', marginBottom: '1em', fontSize: '33px', textAlign: 'center'},
+    subHeader: { marginBottom: '5em', marginInline:'15vw', fontSize: '25px', textAlign: 'center'},
 };
 
 const Spacer = () => <span style={{ width: '1em' }} />;
@@ -45,7 +46,7 @@ const ImgMediaCard: React.FC<ImgMediaCardProps> = ({title, desc, image}) => {
                 <Typography gutterBottom align="center" variant="h5" component="div">
                 {title}
                 </Typography>
-                <Typography align="center" variant="body2" sx={{ color: 'text.secondary' }}>
+                <Typography align="center" variant="body1" sx={{ color: 'text.secondary' }}>
                 {desc}
                 </Typography>
             </CardContent>
@@ -72,20 +73,82 @@ const Home = () => {
     return isXSmall ? (
         <div>
             <div style={styles.flexColumn as CSSProperties}>
+                <div style={styles.header}>
+                    <Typography variant="inherit">
+                        Managing your charging infrastructure or your Vessel fleet has never been easier. Get started with Sea.Energy today.
+                    </Typography>
+                </div>
+                <div style={styles.subHeader}>
+                    <Typography variant="inherit">
+                        Sea.Energy is the operating system driving the Vessel ecosystem, providing companies, captains, charge point operators, a single integrated software solution.
+                    </Typography>
+                </div>
                 <VerticalSpacer />
+                <div style={styles.singleCol}>
+                    <ImgMediaCard
+                        title="Captain's Power Hub"
+                        desc="Simplifying the way you power up your Vessels, this innovative app offers seamless access to charging stations, real-time availability updates"
+                        image="./home-card1.jpg"
+                        />
+                    </div>
                 <VerticalSpacer />
+                <div style={styles.singleCol}>
+                    <ImgMediaCard
+                        title="Port Authority"
+                        desc="Easily manage your charge points, connect, publish and monetize them. Compatable with more than 50 manufacturers."
+                        image="./home-card2.webp"
+                        />
+                    </div>
+                    <VerticalSpacer />
+                    <div style={styles.singleCol}>
+                    <ImgMediaCard
+                        title="Captain's Power Hub"
+                        desc="Simplifying the way you power up your Vessels, this innovative app offers seamless access to charging stations, real-time availability updates"
+                        image="./home-card3.webp"
+                        />
+                    </div>
             </div>
         </div>
     ) : isSmall ? (
         <div style={styles.flexColumn as CSSProperties}>
-            <div style={styles.singleCol}>
+            <div style={styles.header}>
+                <Typography variant="inherit">
+                    Managing your charging infrastructure or your Vessel fleet has never been easier. Get started with Sea.Energy today.
+                </Typography>
+            </div>
+            <div style={styles.subHeader}>
+                <Typography variant="inherit">
+                    Sea.Energy is the operating system driving the Vessel ecosystem, providing companies, captains, charge point operators, a single integrated software solution.
+                </Typography>
             </div>
             <div style={styles.flex}>
-                <Spacer />
-            </div>
-            <div style={styles.singleCol}>
-            </div>
-            <div style={styles.singleCol}>
+                <div style={styles.leftCol}>
+                    <div style={styles.flex}>
+                    <ImgMediaCard
+                        title="Captain's Power Hub"
+                        desc="Simplifying the way you power up your Vessels, this innovative app offers seamless access to charging stations, real-time availability updates"
+                        image="./home-card1.jpg"
+                        />
+                    </div>
+                </div>
+                <div style={styles.midCol}>
+                    <div style={styles.flex}>
+                    <ImgMediaCard
+                        title="Port Authority"
+                        desc="Easily manage your charge points, connect, publish and monetize them. Compatable with more than 50 manufacturers."
+                        image="./home-card2.webp"
+                        />
+                    </div>
+                </div>
+                <div style={styles.rightCol}>
+                    <div style={styles.flex}>
+                    <ImgMediaCard
+                        title="Captain's Power Hub"
+                        desc="Simplifying the way you power up your Vessels, this innovative app offers seamless access to charging stations, real-time availability updates"
+                        image="./home-card3.webp"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     ) : (
@@ -108,7 +171,6 @@ const Home = () => {
                         desc="Simplifying the way you power up your Vessels, this innovative app offers seamless access to charging stations, real-time availability updates"
                         image="./home-card1.jpg"
                         />
-                        {/* <Spacer /> */}
                     </div>
                 </div>
                 <div style={styles.midCol}>
@@ -118,7 +180,6 @@ const Home = () => {
                         desc="Easily manage your charge points, connect, publish and monetize them. Compatable with more than 50 manufacturers."
                         image="./home-card2.webp"
                         />
-                        {/* <Spacer /> */}
                     </div>
                 </div>
                 <div style={styles.rightCol}>
@@ -128,7 +189,6 @@ const Home = () => {
                         desc="Simplifying the way you power up your Vessels, this innovative app offers seamless access to charging stations, real-time availability updates"
                         image="./home-card3.webp"
                         />
-                        {/* <Spacer /> */}
                     </div>
                 </div>
             </div>
